722cc2851aac2e4abc3595e3ef6fa7cb65129a98
Gusta <gustavowustemberg14@gmail.com>
2022-07-10 01:45:23 -0300

    add number auxiliar

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ folderLink = r'{}\\'.format(folder)
 name = input('Digite o novo nome do arquivo: ')
 tipo = input('Digite o tipo do arquivo: ')
 contador = 0
+numaux = ''
 
 for file_name in os.listdir(folderLink):
     contador += 1
+    if(contador < 10):
+        numaux = 0
+    else:
+        numaux = ''
     old_name = folderLink + file_name
-    new_name = folderLink + f'{name}-{contador}.{tipo}'
+    new_name = folderLink + f'{name}-{numaux}{contador}.{tipo}'
     os.rename(old_name, new_name)
 
 print(os.listdir(folderLink))
