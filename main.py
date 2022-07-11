9ce5e1a53bcd5335a685305c58eb557e2cb7af35
Gusta <gustavowustemberg14@gmail.com>
2022-07-10 23:17:18 -0300

    add logica numeroInicial

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ name = input('Digite o novo nome do arquivo: ')
 tipo = input('Digite o tipo do arquivo: ')
 contador = 0
 numaux = ''
+numinicial = int(input('Digite o valor inicial a ser acrescentado ao nome: '))
 
 for file_name in os.listdir(folderLink):
-    contador += 1
+    if(numinicial == 1):
+        contador += 1
+    else:
+        contador = numinicial
+        numinicial += 1
     if(contador < 10):
         numaux = 0
     else:
